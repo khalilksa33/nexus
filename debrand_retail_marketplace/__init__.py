b6bc053a3a2d91b2275f50c8b456c814306028bc
khalilksa33 <khalilksa33@users.noreply.github.com>
2026-06-25 14:38:37 +0300

    Initial commit: Debranded Odoo server setup configured for 10 outlets and 10 vendors

---
+++ debrand_retail_marketplace/__init__.py
@@ -0,0 +1,2 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# This is an empty init file as this module contains xml views and configurations.
---
+++ debrand_retail_marketplace/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2026 Antigravity
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Debranded Retail and Marketplace Setup",
+    "summary": "Debrands the system and configures 10 selling outlets and 10 vendor portal accounts.",
+    "version": "18.0.1.0.0",
+    "category": "Customization",
+    "author": "Antigravity, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/server-brand",
+    "license": "AGPL-3",
+    "depends": [
+        "portal",
+        "point_of_sale",
+        "website_sale",
+        "base_setup",
+    ],
+    "data": [
+        "views/debranding_views.xml",
+        "data/selling_outlets_data.xml",
+        "data/vendor_portal_data.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
