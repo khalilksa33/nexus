d2e9141e00d4a494c9444fa8e3b97f5461b63ce8
khalilksa33 <khalilksa33@users.noreply.github.com>
2026-06-25 16:04:54 +0300

    Trigger deployment with Cloudflare Access Service Auth policy verified

--- debrand_retail_marketplace/__init__.py
+++ debrand_retail_marketplace/__init__.py
@@ -1,3 +1,3 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 # This is an empty init file as this module contains xml views and configurations.
-# Triggering deployment run with Cloudflare Access Service Token configuration.
+# Triggering deployment run with Cloudflare Access Service Auth policy verified.
